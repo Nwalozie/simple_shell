0f59fd593389b772b1c757b51ed3cd95e6002ac9
Tboy54321 <122861724+Tboy54321@users.noreply.github.com>
2023-05-24 23:41:55 +0100

    Create str_cmp.c

---
+++ str_cmp.c
@@ -0,0 +1,27 @@
+#include "shell.h"
+
+/**
+ * str_cmp - compares two strings
+ * @x: first string
+ * @y: second string
+ * Return: the diference between x and y
+ */
+
+int str_cmp(char *x, char *y)
+{
+        while ((*x != '\0' && *y != '\0') && *x == *y)
+        {
+                x++;
+                y++;
+        }
+
+        if (*x == *y)
+        {
+                return (0);
+        }
+
+        else
+        {
+                return (*x - *y);
+        }
+}
