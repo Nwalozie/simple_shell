7b74d92e6478e186835f536a1ef26946c878d4a7
Tboy54321 <122861724+Tboy54321@users.noreply.github.com>
2023-05-24 07:48:45 +0100

    Create shell.c

---
+++ shell.c
@@ -0,0 +1,17 @@
+#include "shell.h"
+
+/**
+ * main - Entry point of the shell program
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ * @env: Array of environment variables
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char **argv, char **env)
+{
+        if (argc == 1)
+                prompt(argv, env);
+
+        return (0);
+}
