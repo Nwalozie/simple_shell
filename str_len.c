c3e8d60827d2afa2ba64b059e3c0dac054ad224b
Tboy54321 <122861724+Tboy54321@users.noreply.github.com>
2023-05-24 23:43:46 +0100

    Create str_len.c

---
+++ str_len.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+
+/**
+ *str_len - return length to a string
+ *@y: The character to count
+ * Return: return an integer "x"
+ */
+
+int str_len(char *y)
+{
+        int x = 0;
+
+        while (y[x] != '\0')
+                x++;
+        return (x);
+}
