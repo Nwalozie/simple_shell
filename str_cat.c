a4ea3b271d2e824bafa40748b1a80ab49f670529
Tboy54321 <122861724+Tboy54321@users.noreply.github.com>
2023-05-24 23:41:03 +0100

    Create str_cat.c

---
+++ str_cat.c
@@ -0,0 +1,22 @@
+#include "shell.h"
+
+/**
+ * str_cat - Concatenate two strings
+ * @dest: array 1
+ * @src: array 2
+ * Return: dest
+ */
+char *str_cat(char *dest, char *src)
+{
+        int x = 0, y;
+
+        while (dest[x] != '\0')
+                x++;
+
+        for (y = 0; src[y] != '\0'; y++, x++)
+        {
+                dest[x] = src[y];
+        }
+
+        return (dest);
+}
