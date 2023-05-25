7f42c718341d780a91845daa29a5afd28c9680c0
Tboy54321 <122861724+Tboy54321@users.noreply.github.com>
2023-05-25 12:38:15 +0100

    Create str_dup.c

---
+++ str_dup.c
@@ -0,0 +1,38 @@
+#include "shell.h"
+
+/**
+ * str_dup - returns a pointer to a newly allocated space in memory.
+ * @str: The string to copy.
+ * Return: The string copy.
+ */
+
+char *str_dup(char *str)
+{
+        char *dest;
+        int i = 0;
+        int b;
+
+        if (str == NULL)
+        {
+                return (NULL);
+        }
+
+        while (str[i])
+        {
+                i++;
+        }
+        i++;
+
+        dest = malloc(sizeof(char) * i);
+
+        if (dest == 0)
+        {
+                return (NULL);
+        }
+
+        for (b = 0; b <= i; b++)
+        {
+                dest[b] = str[b];
+        }
+        return (dest);
+}
