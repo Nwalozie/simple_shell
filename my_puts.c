066694c30992cb57af2217908d9ac0edab3e4cec
Tboy54321 <122861724+Tboy54321@users.noreply.github.com>
2023-05-25 17:43:12 +0100

    Create my_puts.c

---
+++ my_puts.c
@@ -0,0 +1,15 @@
+#include "shell.h"
+
+/**
+ * my_puts - Prints a string to stdout
+ * @str: The string to print
+ */
+
+void my_puts(const char *str)
+{
+        while (*str != '\0')
+        {
+                _putchar(*str);
+                str++;
+        }
+}
