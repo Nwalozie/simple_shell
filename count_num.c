97ff1a6a282f3fcce779eb306443b01a0d751e96
Nwalozie <iyekcnwalozie@gmail.com>
2023-05-26 07:40:26 -0700

    update

---
+++ count_num.c
@@ -0,0 +1,29 @@
+#include "shell.h"
+
+/**
+ * count_num - Counts the number of digits in an integer
+ * @n: The integer to count digits for
+ *
+ * Return: The number of digits in the integer
+ */
+
+int count_num(int n)
+{
+	unsigned int c;
+	int count = 1;
+
+	if (n < 0)
+	{
+		count++;
+		c = n * -1;
+	}
+	else
+		c = n;
+	while (c > 9)
+	{
+		count++;
+		c /= 10;
+	}
+
+	return (count);
+}
